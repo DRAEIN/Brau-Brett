92c1a3fabf391c4901252b8f11710d4712235e96
Mark Billes <mark.billes@web.de>
2018-03-15 23:09:35 +0100

    edited search

--- app/models/job.rb
+++ app/models/job.rb
@@ -11,6 +11,6 @@ class Job < ApplicationRecord
 	end
 
 	def self.search(search)
-		where("Ort LIKE ? OR Titel LIKE ? OR Beschreibung LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
+		where("jobs.Ort LIKE ? OR jobs.Titel LIKE ? OR jobs.Beschreibung LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%")
 	end
 end
