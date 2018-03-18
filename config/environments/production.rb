8b2b68074a5af2daced3011b81e97a92a38fc396
Mark Billes <mark.billes@web.de>
2018-03-18 02:42:47 +0100

    added configs for devise to run in production with heroku/sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Braubrett_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'http://www.brau-brett.de' }
+  ActionMailer::Base.smtp_settings = {
+  :user_name            => ENV['SENDGRID_USERNAME'],
+  :password             => ENV['SENDGRID_PASSWORD'],
+  :address              => "smtp.sendgrid.net",
+  :port                 => 587,
+  :enable_starttls_auto => true,
+  :authentication       => :plain,
+  :domain               => "http://www.brau-brett.de"
+}
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
